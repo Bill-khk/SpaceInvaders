42e8c6db8521eee37ff71cf73ce909f7bb7ebebf
Bill Khk <bill.khk@gmail.com>
2025-04-14 15:03:18 +0700

    Implementing the increase of power and missile number. Function to fix

--- spaceship.py
+++ spaceship.py
@@ -37,19 +37,41 @@ class Spaceship(Turtle):
         self.screen.update()
 
     def auto_shoot(self):
-        if self.shooting:
-            Missile(self)
-            turtle.ontimer(self.auto_shoot, 400)  # fire every 400 ms
-            self.screen.update()
+        if self.shooting: # TODO Correct this
+            for nb_missile in range(1, self.missile+1):
+                if self.missile % 2 == 0:
+                    Missile(self, True, nb_missile+1)
+                else:
+                    Missile(self, False, nb_missile+1)
+        turtle.ontimer(self.auto_shoot, 400)  # fire every 400 ms
+        self.screen.update()
 
 
 class Missile(Turtle):
-    def __init__(self, vessel):
+    def __init__(self, vessel, option, nb):
         super().__init__()
         self.shape('classic')
         self.setheading(90)
         self.penup()
-        self.teleport(x=vessel.xcor(), y=vessel.ycor() + 10)
+        if option:  # If pair
+            if nb % 2 == 0:  # Half are positioned on the left part of the vessel
+                index = -1
+            else:
+                index = 1
+            self.teleport(x=vessel.xcor() + (index * nb * 15), y=vessel.ycor() + 10)
+        else:
+            if nb == 1:
+                self.teleport(x=vessel.xcor(), y=vessel.ycor() + 10)
+                self.color('red')
+            else:
+                if nb % 2 == 0:  # Half are positioned on the left part of the vessel
+                    index = -1
+                    nb_index = nb/2
+                else:
+                    index = 1
+                    nb_index = (nb-1)/2
+                self.teleport(x=vessel.xcor() + (index * nb_index * 15) + (index * 10), y=vessel.ycor() + 10)
+
         self.exist = True
         self.vessel = vessel
         self.move()  # Missile always moves
@@ -77,7 +99,6 @@ class Missile(Turtle):
                         self.random_drop(invader)
                         invader.teleport(1000, 1000)
 
-
     def random_drop(self, invader):
         luck = random.randint(1, 10)
         if luck % 2 == 0:
@@ -92,7 +113,7 @@ class Gift(Turtle):
     def __init__(self, vessel, invader):
         super().__init__()
         self.penup()
-        self.teleport(0,0)
+        self.teleport(0, 0)
         self.teleport(x=invader.xcor(), y=invader.ycor())
         self.shape(self.gift_url)
         self.setheading(270)
@@ -114,7 +135,7 @@ class Gift(Turtle):
 
     def check_pickup(self):
         if self.exist:
-            if self.vessel.xcor() + 20 >= self.xcor() >= self.vessel.xcor() - 20 and self.ycor() <= self.vessel.ycor() + 20:
+            if self.vessel.xcor() + 30 >= self.xcor() >= self.vessel.xcor() - 30 and self.ycor() <= self.vessel.ycor() + 20:
                 # print(f'picked up : {self.vessel.xcor() + 20} >= {self.xcor()} >= {self.vessel.xcor() - 20} and {self.ycor()} <= {self.vessel.ycor() + 20}')
                 self.hideturtle()
                 self.teleport(1000, 1000)
